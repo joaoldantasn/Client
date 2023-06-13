605ac2dae9c71a9d35ffa3bb8db19cd2ae9ef4d1
Joao Lucas <joao.lucas.gomes@maisunifacisa.com.br>
2023-06-13 09:11:34 -0300

    POST Client

--- src/main/java/com/jlucas/client/controllers/ClientController.java
+++ src/main/java/com/jlucas/client/controllers/ClientController.java
@@ -1,16 +1,22 @@
 package com.jlucas.client.controllers;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jlucas.client.dtos.ClientDTO;
 import com.jlucas.client.services.ClientService;
+
 
 
 @RestController
@@ -30,5 +36,13 @@ public class ClientController {
 	public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable){
 		Page<ClientDTO> dto =  service.findAll(pageable);
 		return ResponseEntity.ok(dto);
+	}
+	
+	@PostMapping()
+	public ResponseEntity<ClientDTO> insert( @RequestBody ClientDTO dto) {
+		dto =  service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
